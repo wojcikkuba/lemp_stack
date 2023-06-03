d0c2b77ed3f7a6da5231ce079af4d02a493ec2be
wojcikkuba <wojcikjakub@int.pl>
2023-06-03 16:44:36 +0200

    Clean up

--- src/index.php
+++ src/index.php
@@ -8,16 +8,16 @@
 	<h3>LEMP Stack Test | Jakub Wojcik</h3>
 	<hr>
 
+	<!--Read from database>
 	<?php
         $connection = new PDO('mysql:host=mysql;dbname=lemp_test;charset=utf8', 'root', 'password');
         $query      = $connection->query("SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'lemp_test'");
         $tables     = $query->fetchAll(PDO::FETCH_COLUMN);
 
         if (empty($tables)) {
-            echo '<p class="center">There are no tables in database <code>demo</code>.</p>';
+            echo '<pThere are no tables in database <code>lemp_stack</code>.</p>';
         } else {
-            echo '<p class="center">Database <code>demo</code> contains the following tables:</p>';
-            echo '<ul class="center">';
+            echo '<p class="center">Database <code>lemp_stack</code> contains the following tables:</p>';
             foreach ($tables as $table) {
                 echo "<li>{$table}</li>";
             }
